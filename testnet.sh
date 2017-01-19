3acf8625cedb5f5e88c65993a72e1ec89e64663c
dinstein <lianglinfeng@gmail.com>
2017-01-19 14:54:57 +0800

    add "sudo"

--- testnet.sh
+++ testnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker volume create --name=bitcoind-data-testnet
+sudo docker volume create --name=bitcoind-data-testnet
 
-docker run -v bitcoind-data-testnet:/bitcoin --name=bitcoind-node-testnet -d \
+sudo docker run -v bitcoind-data-testnet:/bitcoin --name=bitcoind-node-testnet -d \
         -p 18333:18333 \
         -p 127.0.0.1:18332:18332 \
-        -v testnet-bitcoin.conf:/bitcoin/.bitcoin/bitcoin.conf \
+        -v $(pwd)/testnet-bitcoin.conf:/bitcoin/.bitcoin/bitcoin.conf \
         dinstein/bitcoind
 
-# docker logs -f bitcoind-node-testnet
+# sudo docker logs -f bitcoind-node-testnet
