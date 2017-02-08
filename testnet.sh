0e9e20f3695b1ffe99e243afceab64ffd0bb04d6
dinstein <lianglinfeng@gmail.com>
2017-02-08 15:53:16 +0800

    allow ip 192.168.0.1/16

--- testnet.sh
+++ testnet.sh
@@ -5,6 +5,7 @@ sudo docker volume create --name=bitcoind-data-testnet
 sudo docker run -v bitcoind-data-testnet:/bitcoin --name=bitcoind-node-testnet -d \
         -p 18333:18333 \
         -p 127.0.0.1:18332:18332 \
+        -p 192.168.0.1:18332:18332 \
         -v $(pwd)/testnet-bitcoin.conf:/bitcoin/.bitcoin/bitcoin.conf \
         dinstein/bitcoind
 
