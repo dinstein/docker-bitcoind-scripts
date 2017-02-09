cef19079a0f974be35bdc5a5988fd574fb835c36
dinstein <lianglinfeng@gmail.com>
2017-02-09 16:45:30 +0800

    remove sudo

--- mainnet.sh
+++ mainnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo docker volume create --name=bitcoind-data-mainnet
+docker volume create --name=bitcoind-data-mainnet
 
-sudo docker run -v bitcoind-data-mainnet:/bitcoin --name=bitcoind-node-mainnet -d \
+docker run -v bitcoind-data-mainnet:/bitcoin --name=bitcoind-node-mainnet -d \
         -p 8333:8333 \
         -p 127.0.0.1:8332:8332 \
         -v $(pwd)/mainnet-bitcoin.conf:/bitcoin/.bitcoin/bitcoin.conf \
         dinstein/bitcoind
 
-# sudo docker logs -f bitcoind-node-mainnet
+# docker logs -f bitcoind-node-mainnet
--- testnet.sh
+++ testnet.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo docker volume create --name=bitcoind-data-testnet
+# sudo usermod -aG docker {username}
 
-sudo docker run -v bitcoind-data-testnet:/bitcoin --name=bitcoind-node-testnet -d \
+docker volume create --name=bitcoind-data-testnet
+
+docker run -v bitcoind-data-testnet:/bitcoin --name=bitcoind-node-testnet -d \
         -p 18333:18333 \
         -p 127.0.0.1:18332:18332 \
         -p 192.168.0.1:18332:18332 \
         -v $(pwd)/testnet-bitcoin.conf:/bitcoin/.bitcoin/bitcoin.conf \
         dinstein/bitcoind
 
-# sudo docker logs -f bitcoind-node-testnet
+# docker logs -f bitcoind-node-testnet
